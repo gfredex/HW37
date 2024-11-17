4dc884850513ffbb6aa14d6f4df86ee58d66d3be
Dima Zemnitski <g.fredex@gmail.com>
2024-11-18 00:28:49 +0300

    feat: added all  plugins for task

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const { clear } = require('console');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { title } = require('process');
+// const { Template } = require('webpack');
 
 module.exports = {
     entry: {
@@ -9,7 +12,23 @@ module.exports = {
     // "./src/script/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: 'index.js',
         clean: true
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "webpack test",
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+    mode: "development",
+    // mode: "production",
 }
